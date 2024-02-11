5e8bb54cb510180a5ae44d2bc5f2c96da8996fe4
trymeyourself <trymeyourself@gmail.com>
2024-02-11 08:35:12 +0530

    added firstcdemo project files to session 01

---
+++ C/Session-01/firstcdemo/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hi Trainees..! Welcome to TMYS C Tutorials\n");
+    printf("Hello world!\t in c\n");
+
+    int a=10;
+    printf("%d\n",a);
+
+    printf("%d\n",5);
+    return 0;
+}
